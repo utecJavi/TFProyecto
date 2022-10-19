4295c0e3e3672c70f0b7910ae1cb38557070c44f
Javier Asuaga <javier.asuaga@estudiantes.utec.edu.uy>
2022-10-18 22:53:11 -0300

    Subiendo modificaciones en el .gitignore para que no tome la carpeta build y primer boceto de menu

--- TecnoFenixUI/src/tecnofenix/ui/Principal.java
+++ TecnoFenixUI/src/tecnofenix/ui/Principal.java
@@ -12,9 +12,12 @@ import tecnofenix.EJBRemotos.EJBUsuarioRemoto;
 //import tecnocanarios.dao.*;
 
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Principal {
 
@@ -24,19 +27,21 @@ public class Principal {
 //	private JButton btnUIRol;
 //	private JButton btnAdministracionDeRoles;
 
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Principal window = new Principal();
-					
+
 					UILogin uiLogin = new UILogin();
 					uiLogin.inicializar(window);
 //					window.frame.setVisible(true);
 					EJBUsuarioRemoto ejbusu = new EJBUsuarioRemoto();
 					ejbusu.ejecutarMetodo();
-					
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,13 +49,16 @@ public class Principal {
 		});
 	}
 
-
-
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	public Principal() {
-//		inicializar();
+		inicializar();
 	}
 
-
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	public void inicializar() {
 
 		System.out.println("Programa inicializado");
@@ -58,6 +66,36 @@ public class Principal {
 		frame.setBounds(100, 100, 676, 326);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		JMenu jmenu, submenu;
+		JMenuItem menuUsuario, i2, i3, i4, i5;
+		jmenu = new JMenu("Menu");
+		submenu = new JMenu("Sub Menu");
+		menuUsuario = new JMenuItem("Usuario");
+
+		i2 = new JMenuItem("Item 2");
+		i3 = new JMenuItem("Item 3");
+		i4 = new JMenuItem("Item 4");
+		i5 = new JMenuItem("Item 5");
+		jmenu.add(menuUsuario);
+		jmenu.add(i2);
+		jmenu.add(i3);
+		submenu.add(i4);
+		submenu.add(i5);
+		jmenu.add(submenu);
+		menuBar.add(jmenu);
+		frame.setJMenuBar(menuBar);
+
+		//Aca abrimos la ventana de usuario 
+		menuUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Abriendo ventana usuarios");
+			}
+		});
+		
+		
 		
 //		btnUIPersona = new JButton("Personas");
 //		btnUIPersona.addActionListener(new ActionListener() {
