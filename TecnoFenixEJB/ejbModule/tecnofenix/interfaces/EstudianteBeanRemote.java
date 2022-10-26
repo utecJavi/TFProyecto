1737069f5b34c07b959ed96da1b627fa19f64cac
Javier Asuaga <javier.asuaga@estudiantes.utec.edu.uy>
2022-10-26 20:56:13 -0300

    Backend entidad Estudiante remote y servicios

--- TecnoFenixEJB/ejbModule/tecnofenix/interfaces/EstudianteBeanRemote.java
+++ TecnoFenixEJB/ejbModule/tecnofenix/interfaces/EstudianteBeanRemote.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import javax.ejb.Remote;
 
+import tecnofenix.entidades.Constancia;
+import tecnofenix.entidades.ConvocatoriaAsistenciaEventoEstudiante;
 import tecnofenix.entidades.Estudiante;
+import tecnofenix.entidades.Justificacion;
+import tecnofenix.entidades.Reclamo;
 import tecnofenix.exception.ServiciosException;
 
 @Remote
@@ -12,6 +16,9 @@ public interface EstudianteBeanRemote {
 	Estudiante crearEstudiante(Estudiante estudiante) throws ServiciosException;
 	Estudiante modificarEstudiante(Estudiante estudiante) throws ServiciosException;
 	Estudiante borrarEstudiante(Estudiante estudiante) throws ServiciosException;
-	List<Estudiante> obtenerEstudiantePorAtributo(Estudiante estudiante);
-	
+	Estudiante obtenerEstudiantePorAtributo(String documento)throws ServiciosException;
+	List<Estudiante> listarAsistenciasAEventos(Integer eventoId)throws ServiciosException;
+	List<Estudiante> listarJustificaciones(Integer justificacionId)throws ServiciosException;
+	List<Estudiante> lisatEstudiantePorReclamos(Integer reclamoId)throws ServiciosException;
+	List<Estudiante> listarConstancias(Integer constanciaId)throws ServiciosException;
 }
--- TecnoFenixEJB/ejbModule/tecnofenix/servicios/EstudianteBean.java
+++ TecnoFenixEJB/ejbModule/tecnofenix/servicios/EstudianteBean.java
@@ -9,9 +9,9 @@ import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 
 import tecnofenix.entidades.Estudiante;
+import tecnofenix.entidades.Usuario;
 import tecnofenix.exception.ServiciosException;
 import tecnofenix.interfaces.EstudianteBeanRemote;
-
 
 /**
  * Session Bean implementation class CarrerasBean
@@ -20,24 +20,28 @@ import tecnofenix.interfaces.EstudianteBeanRemote;
 public class EstudianteBean implements EstudianteBeanRemote {
 	@PersistenceContext
 	private EntityManager em;
-	
-    /**
-     * Default constructor. 
-     */
-    public EstudianteBean() {
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * Default constructor.
+	 */
+	public EstudianteBean() {
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public Estudiante crearEstudiante(Estudiante estudiante) throws ServiciosException {
-		// TODO Auto-generated method stub
-		return null;
+
+		estudiante = em.merge(estudiante);
+
+		return estudiante;
 	}
 
 	@Override
 	public Estudiante modificarEstudiante(Estudiante estudiante) throws ServiciosException {
-		// TODO Auto-generated method stub
-		return null;
+		if (estudiante != null) {
+			estudiante = em.merge(estudiante);
+		}
+		return estudiante;
 	}
 
 	@Override
@@ -47,9 +51,88 @@ public class EstudianteBean implements EstudianteBeanRemote {
 	}
 
 	@Override
-	public List<Estudiante> obtenerEstudiantePorAtributo(Estudiante estudiante) {
-		// TODO Auto-generated method stub
-		return null;
+	public Estudiante obtenerEstudiantePorAtributo(String documento) {
+
+		TypedQuery<Estudiante> query = em
+				.createQuery("SELECT e FROM Estudiante e WHERE e.documento = :documento", Estudiante.class)
+				.setParameter("documento", documento);
+		em.flush();
+		System.out.println("ESTUDIANTEBEAN LUEGO DE LA QUERY obtenerEstudiantePorAtributo");
+
+		return query.getSingleResult();
+
+	}
+
+	@Override
+	public List<Estudiante> listarAsistenciasAEventos(Integer eventoId) throws ServiciosException {
+		TypedQuery<Estudiante> query = em.createQuery(
+				"SELECT e FROM Estudiante e INNER JOIN e.convocatoriaAsistenciaEventoEstudianteCollection event WHERE event.eventoId = :eventoId",
+				Estudiante.class).setParameter("eventoId", eventoId);
+
+		for (int i = 0; i < query.getResultList().size(); i++) {
+			System.out.println(query.getResultList().get(0).toString());
+		}
+//		System.out.println(query.getResultList().get(0).getId());
+//		System.out.println(query.getResultList().get(0).getNombres());
+//		System.out.println(query.getResultList().get(0).getApellidos());
+//		System.out.println(query.getResultList().get(0).getDocumento());
+		em.flush();
+		System.out.println("ESTUDIANTEBEAN LUEGO DE LA QUERY listarAsistenciasAEventos");
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Estudiante> listarJustificaciones(Integer justificacionId) throws ServiciosException {
+		TypedQuery<Estudiante> query = em.createQuery(
+				"SELECT e FROM Estudiante e INNER JOIN e.justificacionCollection event WHERE event.eventoId = :eventoId",
+				Estudiante.class).setParameter("eventoId", justificacionId);
+
+		for (int i = 0; i < query.getResultList().size(); i++) {
+			System.out.println(query.getResultList().get(0).toString());
+		}
+//		System.out.println(query.getResultList().get(0).getId());
+//		System.out.println(query.getResultList().get(0).getNombres());
+//		System.out.println(query.getResultList().get(0).getApellidos());
+//		System.out.println(query.getResultList().get(0).getDocumento());
+		em.flush();
+		System.out.println("ESTUDIANTEBEAN LUEGO DE LA QUERY listarJustificaciones");
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Estudiante> lisatEstudiantePorReclamos(Integer reclamoId) throws ServiciosException {
+		TypedQuery<Estudiante> query = em.createQuery(
+				"SELECT e FROM Estudiante e INNER JOIN e.reclamoCollection event WHERE event.eventoId = :eventoId",
+				Estudiante.class).setParameter("eventoId", reclamoId);
+
+		for (int i = 0; i < query.getResultList().size(); i++) {
+			System.out.println(query.getResultList().get(0).toString());
+		}
+//		System.out.println(query.getResultList().get(0).getId());
+//		System.out.println(query.getResultList().get(0).getNombres());
+//		System.out.println(query.getResultList().get(0).getApellidos());
+//		System.out.println(query.getResultList().get(0).getDocumento());
+		em.flush();
+		System.out.println("ESTUDIANTEBEAN LUEGO DE LA QUERY lisatEstudiantePorReclamos");
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Estudiante> listarConstancias(Integer constanciaId) throws ServiciosException {
+		TypedQuery<Estudiante> query = em.createQuery(
+				"SELECT e FROM Estudiante e INNER JOIN e.constanciaCollection event WHERE event.eventoId = :eventoId",
+				Estudiante.class).setParameter("eventoId", constanciaId);
+
+		for (int i = 0; i < query.getResultList().size(); i++) {
+			System.out.println(query.getResultList().get(0).toString());
+		}
+//		System.out.println(query.getResultList().get(0).getId());
+//		System.out.println(query.getResultList().get(0).getNombres());
+//		System.out.println(query.getResultList().get(0).getApellidos());
+//		System.out.println(query.getResultList().get(0).getDocumento());
+		em.flush();
+		System.out.println("ESTUDIANTEBEAN LUEGO DE LA QUERY listarConstancias");
+		return query.getResultList();
 	}
 
 }
